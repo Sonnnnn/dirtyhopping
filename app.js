a80d1a605d8f7b5d38e6048f13e89d95a5e5b303
Son <sitthavee.t@gmail.com>
2021-05-11 18:40:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,22 +9,13 @@ var base = Airtable({apiKey: 'keyqBkxeEokhp8v71'}).base('appSb8rYBk8f1qWcj');
 
 var table = base('CafeList');
 
-table.select({
-  // Selecting the first 3 records in Grid view:
-  maxRecords: 100,
-  view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-  // This function (`page`) will get called for each page of records.
-
-  records.forEach(function(record) {
+table.select({maxRecords: 100, view: "Grid view"}).eachPage(
+  function page(records, fetchNextPage) {
+    records.forEach(function(record) {
       console.log('Retrieved', record.get('Place ID'));
   });
-
-  // To fetch the next page of records, call `fetchNextPage`.
-  // If there are more records, `page` will get called again.
-  // If there are no more records, `done` will get called.
   fetchNextPage();
-
+  }
 }, function done(err) {
   if (err) { console.error(err); return; }
 });
